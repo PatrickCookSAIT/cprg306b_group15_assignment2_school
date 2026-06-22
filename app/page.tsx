85153c577f25254b84df9b37bebb2034a4dff717
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-06-22 10:20:48 -0600

    col => cols typo

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import StudentCard from '@/components/StudentCard'
 const Home = async () => {
   const students = await getAllStudents()
   return (
-    <div className = "grid grid-cols-1 md:grid-col-2 lg_grid-col-3 gap-6">
+    <div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {students.map((student)=>(
         <StudentCard key={student.id} student={student}/>
       ))}
