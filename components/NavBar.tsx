9b8d5dea9822c0613e1f5028545fbb976c3705e5
V-Weaver <ash.v.weaver@proton.me>
2026-06-23 11:19:09 -0600

    added footer and header

---
+++ components/NavBar.tsx
@@ -0,0 +1,16 @@
+const Navbar = () => {
+  return (
+  <>
+    <div className = "text-slate-700 py-2.5 px-4 text-lg font-bold">NGHS</div>
+    <nav className = "text-slate-50 flex gap-4 bg-slate-700 mb-6 py-1.5 px-4">
+      <div><a href="#" >Students</a></div>
+      <div><a href="#" >Calendar</a></div>
+      <div><a href="#" >News</a></div>
+    </nav>
+
+
+  </>
+  );
+};
+
+export default Navbar;
