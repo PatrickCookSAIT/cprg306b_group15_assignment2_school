ef5c7afb1a637e7c348af5c0d5cd0c1e2e26f763
PatrickCookSAIT <patrick.cook@edu.sait.ca>
2026-06-25 14:15:53 -0600

    documentation

--- app/page.tsx
+++ app/page.tsx
@@ -9,13 +9,14 @@ const Home = async () => {
   const students = await getAllStudents()
   return (
     <>
+    {/*map through all student data and display the data using StudentCard component */}
     <div className = "grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
       {students.map((student)=>(
         <StudentCard key={student.id} student={student}/>
       ))}
 
     </div>
-
+      {/*load StudentForm component */}
     <StudentForm/>
     </>
   )
