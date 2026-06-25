cdc2af956d8223c561e8717b76d231e2c320c2f3
V-Weaver <ash.v.weaver@proton.me>
2026-06-25 14:51:19 -0600

    added some comments

--- lib/actions.ts
+++ lib/actions.ts
@@ -1,3 +1,16 @@
+/*
+ * Authors: Ash Burn, Patrick Cook
+ * Date: June 24th, 2026
+ * Program: Student data manipulation, server actions
+ *
+ * Description:
+ * Server side actions for managing the data of students. These run on the server so that they
+ * may read and write files. The input is validated form data for a new student. The data is re
+ * validated on this side with the Zod schema. Loads the current list, assigns a new id based
+ * on length of list, appends the new student. Outputs to students.json and sends a result on whether
+ * the save succeeded.
+ */
+
 "use server";
 
 import { revalidatePath } from "next/cache";
